83826f0d781f08488da6f7cdfb1fe7d481700149
unknown <vdnam.ute@gmail.com>
2023-12-15 15:20:21 +0700

    Testing

--- setup.sh
+++ setup.sh
@@ -6,5 +6,4 @@ gnome-terminal --working-directory=/home/admin1/Desktop/dws_record/ -x bash -c "
     sudo apt-get install python3-pip;sleep 1; \
     pip3 install pynput;sleep 1; \
     sudo pip install pyserial; sleep 1;\
-    cp -i /home/admin1/Desktop/dws_record/auto-start-setup.desktop ~/.config/autostart; sleep 1; \
-    shutdown -r now"
+    cp -i /home/admin1/Desktop/dws_record/auto-start-setup.desktop ~/.config/autostart"
