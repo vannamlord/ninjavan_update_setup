1f429b99a986bf9c702b1eba9cb1ef3f85699c43
unknown <vdnam.ute@gmail.com>
2023-12-18 13:08:25 +0700

    update dws type

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-gnome-terminal --working-directory=/home/admin1/Desktop/dws_record/ -x bash -c " \
-    sudo apt update; \
-    sudo apt-get install git -y;sleep 1; \
-    git init;sleep 1; \
-    sudo apt-get install python3-pip -y;sleep 1; \
-    pip3 install pynput;sleep 1; \
-    sudo pip install pyserial; sleep 1;\
-    cp -i /home/admin1/Desktop/dws_record/auto-start-setup.desktop ~/.config/autostart -y; sleep 1; \
-    python3 check_update.py"
+sudo apt update
+sudo apt-get install git -y
+git init
+sudo apt-get install python3-pip -y
+pip3 install pynput
+sudo pip install pyserial
+cp -i /home/admin1/Desktop/dws_record/auto-start-setup.desktop ~/.config/autostart -y
+echo -n "PLEASE ENTER TYPE OF MACHINE (DWS/BDWS): "
+read machine_type
+echo "$machine_type" > machine_type.txt
+python3 check_update.py
